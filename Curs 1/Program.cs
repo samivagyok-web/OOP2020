13d122793b5c2ed12f6b48439e3ae327c9a66e5d
samivagyok-web <samivagyok@gmail.com>
2021-02-25 11:01:44 +0200

    add Constructor, Static demo, SayHello

--- Curs 1/Program.cs
+++ Curs 1/Program.cs
@@ -10,7 +10,18 @@ namespace Curs_1
     {
         static void Main(string[] args)
         {
+            World w1 = new World(1);
+            w1.SayHello();
 
+            World w2 = new World(2);
+            w2.SayHello();
+
+            for (int i = 0; i < 100; i++)
+            {
+                new World(new Random().Next(0, 1000));
+            }
+
+            Console.WriteLine(World.Counter);
         }
     }
 }
---
+++ Curs 1/World.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Curs_1
+{
+    class World
+    {
+        /// <summary>
+        /// Numarul de identificare al -World-
+        /// Nefiind static el este de tip instanta si va exista cate unul pt fiecare obiect creat
+        /// Spunem ca este o variabila la nivel de obiect
+        /// </summary>
+        private int id;
+
+        /// <summary>
+        /// Tine evidenta numarului obiecte de tip World create.
+        /// Fiind static va exista o singura valoare pentru toate obiectele create.
+        /// Spunem ca este o variabila la nivel de clasa
+        /// </summary>
+        private static int counter = 0;
+
+        /// <summary>
+        /// Metoda constructor.
+        /// Are acelasi nume ca si clasa
+        /// Nu are tip return, nici macar void
+        /// Initializeaza obiectul.
+        /// Se apeleaza automat la crearea/instantierea unui obiect
+        /// </summary>
+        /// <param name="id"></param>
+        public World(int id)
+        {
+            this.id = id;
+            counter++;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hello from World {id}");
+        }
+
+        public static int Counter
+        {
+            get
+            {
+                return counter;
+            }
+        }
+    }
+}
