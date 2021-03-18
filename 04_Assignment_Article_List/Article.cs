471415b10ce38fbe99de076533063708a68c7747
samivagyok-web <samivagyok@gmail.com>
2021-03-18 20:06:17 +0200

    Article class

---
+++ 04_Assignment_Article_List/Article.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Assignment_Article_List
+{
+    class Article
+    {
+        #region Private Members
+        private string title, content, tag;
+        private List<string> authors = new List<string>();
+        DateTime datePosted, dateLastUpdate;
+        private static Random rnd = new Random();
+        #endregion
+
+        #region Constructor
+        public Article(string title, string content, List<string> authors, DateTime datePosted, DateTime dateLastUpdate, string tag)
+        {
+            this.title = title;
+            this.content = content;
+            this.authors = authors;
+            this.datePosted = datePosted;
+            this.dateLastUpdate = dateLastUpdate;
+            this.tag = tag;
+        }
+        #endregion
+
+        #region Likes/Dislikes
+        private int likes { get; } = rnd.Next(0, 10000);
+        private int dislikes { get; } = rnd.Next(0, 10000);
+        #endregion
+
+        #region Properties
+        public string Title { get { return title; } }
+        public string Content { get { return content; } }
+        public DateTime DatePosted { get { return datePosted; } }
+        public DateTime DateLastUpdate { get { return dateLastUpdate; } }
+        #endregion
+
+        public string returnAuthorsInString()
+        {
+            string auth = "";
+
+            for (int i = 0; i < authors.Count - 1; i++)
+                auth += authors[i] + ", ";
+            auth += authors[authors.Count - 1];
+
+            return auth;
+        }
+
+        public override string ToString()
+        {
+            return $"{title} by {returnAuthorsInString()} \nDate Posted: {datePosted}\n\n{content}\n\n";
+        }
+    }
+}
---
+++ 04_Assignment_Article_List/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _04_Assignment_Article_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader file = new StreamReader("Arcticles.txt");
+            List<Article> articles = new List<Article>();
+
+            while (file.ReadLine() != null)
+            {
+                string title = file.ReadLine();
+
+                var authorList = new List<string>();
+                string authors = file.ReadLine();
+
+                if (authors.Contains(','))
+                {
+                    string[] names = authors.Split(',');
+
+                    for (int i = 0; i < names.Length; i++)
+                        authorList.Add(names[i].Trim());
+                }
+                else
+                    authorList.Add(authors);
+
+                string content = file.ReadLine();
+
+                DateTime datePosted = DateTime.Parse(file.ReadLine());
+                DateTime dateUpdated = DateTime.Parse(file.ReadLine());
+
+                string tag = file.ReadLine();
+
+                articles.Add(new Article(title, content, authorList, datePosted, dateUpdated, tag));
+            }
+        }
+    }
+}
